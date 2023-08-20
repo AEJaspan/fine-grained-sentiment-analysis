ef04926fb7cacef5e5de3806fda8056355f4ebe6
Adam Jaspan <adam.jaspan@googlemail.com>
2023-08-20 12:34:53 +0100

    Customising pipeline.

--- driver.py
+++ driver.py
@@ -10,21 +10,33 @@
 # French 	140k
 # Italian 	72k
 # Spanish 	50k
+
+
+# TODO: 
+# Truncation strategy
+# Running in batch mode
+# Fine tuning with the review label
+# Build alternative method
+
 #%%
+
 import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
+import torch
 tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
 model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-# sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 from langdetect import detect
 from langdetect import DetectorFactory
 # https://stats.stackexchange.com/questions/338904/measures-of-ordinal-classification-error-for-ordinal-regression
 from imblearn.metrics import macro_averaged_mean_absolute_error
+from torch.nn import Softmax
+
+smax = Softmax(dim=-1)
 
 # NOTE:
 # ValueError: Found array with 0 sample(s) (shape=(0,)) while a minimum of 1 is required.
@@ -48,10 +60,18 @@ print(data['score'].value_counts())
 data=data[~data['review'].duplicated(keep=False)]
 print(data.shape)
 print(data)
-data=data[:100]
+data=data[:50]
 text_columns = ['review_summary', 'review']
 data['language'] = data['review_summary'].apply(lambda x: detect(x) if x!='' else np.nan)
 data['language'].unique()
+
+def clean_response(response):
+    response=response[0]
+    response['label'] = int(response['label'].split(' ')[0])
+    response['sentiment_score'] = response.pop('label')
+    response['classification_confidence'] = response.pop('score')
+    return pd.Series(response, index=['sentiment_score','classification_confidence'])
+
 #%%
 d=data.copy()
 
@@ -62,21 +82,35 @@ def f(x):
         review_summary=x['review_summary']
         tokens = tokenizer(review_summary, padding=True, truncation=True, return_tensors="pt")
         print(len(tokens.input_ids))
-    return model(**tokens)
-d.apply(f, axis=1)
+    with torch.no_grad():
+        logits = model(**tokens, return_dict=True).logits
+    probs = model(**tokens, return_dict=True).logits.softmax(-1)[0]
+    labels = model.config.id2label
+    predicted_class_id = probs.argmax().item()
+    classification = labels[predicted_class_id]
+    classification_score = probs[predicted_class_id].item()
+    return pd.Series({
+        'sentiment_score': int(classification.split(' ')[0]),
+        'classification_confidence': classification_score
+    }, index=['sentiment_score','classification_confidence'])
+    # outputs = model(**tokens, return_dict=True)
+    # probs0 = smax(outputs.logits)
+    # probs0 = probs0.flatten().detach().numpy()
+
+    # prob_pos = probs[1]
+    # return model(**tokens, return_dict=True)
+d[['sentiment_score','classification_confidence']] = d.apply(f, axis=1)
+d['error'] = np.abs(d['score'] - d['sentiment_score'])
+d
 #%%
-def clean_response(response):
-    response=response[0]
-    response['label'] = int(response['label'].split(' ')[0])
-    response['sentiment_score'] = response.pop('label')
-    response['classification_confidence'] = response.pop('score')
-    return pd.Series(response, index=['sentiment_score','classification_confidence'])
+d=data.copy()
 def f(x):
-    tokens = tokenizer(x)
+    review=x['review']
+    tokens = tokenizer(review)
     if len(tokens.input_ids) > 545:
+        review=x['review_summary']
         print(len(tokens.input_ids))
-    return model(tokens)
-    # return clean_response(sentiment_pipeline(x, truncation=True))
+    return clean_response(sentiment_pipeline(review, truncation=True))
 d[['sentiment_score','classification_confidence']] = d['review'].apply(f)
 d['error'] = np.abs(d['score'] - d['sentiment_score'])
 
